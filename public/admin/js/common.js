e375a072e90fd642da1d8540d0f121dd33317953
Clouds <416259557@qq.com>
2017-11-30 08:48:58 +0800

    基本完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -8,58 +8,45 @@ $(document).ajaxStart(function () {
 });
 
 $(document).ajaxStop(function () {
-  //完成进度条
   setTimeout(function () {
     NProgress.done();
-  }, 500);
+  }, 300);
 });
 
-
-//非登陆页面，判断当前用户是否是登录了，如果登录了，就继续，如果没登陆，需要跳转到登录页面。
-if(location.href.indexOf("login.html") == -1){
+if(location.href.indexOf("login.html")== -1){
   $.ajax({
     type:"get",
     url:"/employee/checkRootLogin",
-    success:function (data) {
+    success:function(data){
       if(data.error === 400){
-        //说明用户没有登录，跳转到登录页面
-        location.href = "login.html";
+        //说明用户没有登录,跳转到登录页面
+        location.herf = "login.html";
       }
     }
   })
 }
-
-
+//侧边栏显示隐藏功能
+$(".icon_menu").on("click",function(){
+  $(".lt_aside").toggleClass("now");
+  $(".lt_mian").toggleClass("now");
+  // $(".topbar").toggleClass("now");
+});
 
 //二级分类显示隐藏功能
-$(".child").prev().on("click", function () {
+$(".child").prev().on("click",function(){
   $(this).next().slideToggle();
 });
 
-//侧边栏显示隐藏功能
-$(".icon_menu").on("click", function () {
-  $(".lt_aside").toggleClass("now");
-  $(".lt_main").toggleClass("now");
-  $(".lt_topbar").toggleClass("now");
-});
-
-
-//退出功能
-$(".icon_logout").on("click", function () {
-  $("#logoutModal").modal("show");
-
-
-  //给退出按钮注册事件, off:解绑所有的事件
-  $(".btn_logout").off().on("click", function () {
-    //console.log("Hehe");
-    //发送ajax请求，退出系统
+// 退出功能
+$(".icon_logout").on("click",function(){
+  $('#myModal').modal('show');  
+  $(".btn_logout").off().on("click",function(){
     $.ajax({
       type:"get",
       url:"/employee/employeeLogout",
       success:function (data) {
         if(data.success){
-          //退出成功
-          location.href = "login.html";
+          location.href="login.html";
         }
       }
     });
--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,114 +1,82 @@
-/**
- * Created by HUCC on 2017/11/21.
- */
-$(function () {
-  
-    //表单校验的功能
-    //1. 用户名不能为空
-    //2. 用户密码不能为空
-    //3. 用户密码的长度是6-12位
-  
-    //如何使用表单校验插件：
-    //1. 引包
-    //2. 调用bootstrapValidator
-    var $form = $("form");
-    $form.bootstrapValidator({
-  
-      //配置校验时的图标,
-      feedbackIcons: {
-        //校验成功的图标
-        valid: 'glyphicon glyphicon-ok',
-        invalid:'glyphicon glyphicon-remove',
-        validating: 'glyphicon glyphicon-refresh'
+$(function(){
+  var $form = $("form");
+  $form.bootstrapValidator({
+    //在调用了bootstrapValidator方法初始化了表单校验之后.
+    //可以通过$form.bootsrapValidator 可以得到一个插件对象
+    //通过对象可以调用插件给我们提供的方法.
+    
+    //配置校验时的图标,
+    feedbackIcons: {
+      //校验成功的图标
+      valid: 'glyphicon glyphicon-ok',
+      invalid:'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+
+    //配置参数
+    //配置校验规则,需要校验的字段,对应表单中的nama属性
+    fields:{
+      username:{
+        validators:{
+          //配置username所有的校验规则
+          //非空校验
+          notEmpty:{
+            message:"用户名不能为空"
+          },
+          callback:{
+            message:"用户名不存在"
+          }
+        }
       },
-  
-      //配置校验的规则
-      //字段，你想要校验哪些字段
-      fields: {
-        //username对应的表单中name属性。
-        username: {
-          //username的规则
-          validators: {
-            notEmpty: {
-              message: "用户名不能为空"
-            },
-            callback: {
-              message:"用户名不存在"
-            }
+      password:{
+        validators:{
+          notEmpty:{
+            message:"密码不能为空"
+          },
+          stringLength:{
+            min:6,
+            max:12,
+            message:"密码长度在6-12位"
+          },
+          callback:{
+            message:"密码错误"
           }
-  
-        },
-        password: {
-  
-          //password的规则
-          validators: {
-            notEmpty: {
-              message: "用户密码不能为空"
-            },
-            stringLength: {
-              min: 6,
-              max: 12,
-              message: "密码长度是6-12位"
-            },
-            callback: {
-              message:"密码错误"
-            }
-          }
-  
         }
       }
-  
-    });
-  
-  
-  
-    //需要给表单注册一个校验成功的事件  success.form.bv
-    $form.on("success.form.bv", function (e) {
-  
-      //阻止浏览器的默认行为
-      e.preventDefault();
-  
-      //发送ajax
-      console.log("嘿嘿嘿");
-      $.ajax({
-        type:"post",
-        url:"/employee/employeeLogin",
-        data: $form.serialize(),
-        success:function (data) {
-          //如果成功，就跳转到首页
-          if(data.success){
-            location.href = "index.html";
-          }
-  
-          if(data.error === 1000){
-            //alert("用户名不存在");
-  
-            //手动调用方法，updateStatus让username校验失败即可
-            //第一个参数：改变哪个字段
-            //第二个参数：改成什么状态  VALID:通过  INVALID:不通过
-            //第三个参数：选择提示的信息
-            $form.data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
-          }
-  
-          if(data.error === 1001){
-            //alert("密码错误");
-  
-            $form.data("bootstrapValidator").updateStatus("password", "INVALID", "callback");
-          }
-        }
-  
-      });
-  
-    });
-  
-  
-    //重置功能，重置样式
-    $("[type='reset']").on("click", function () {
-  
-      //重置样式
-      $form.data("bootstrapValidator").resetForm();
-  
-    });
-  
-  
+    }
+ 
   });
+
+  //给表单注册校验成功事件
+  $form.on("success.form.bv", function (e) {
+    //阻止浏览器默认行为
+    e.preventDefault();
+    console.log("xxx");
+
+    $.ajax({
+      type:"post",
+      url:"/employee/employeeLogin",
+      data: $form.serialize(),
+      success:function (data){
+        if(data.success){
+          //跳转到首页
+          location.href ="index.html";
+        }
+        if(data.error == 1000){
+          //把用户名的校验变为失败
+          $form.data("bootstrapValidator").updateStatus("username","INVALID","callback");
+
+        }
+        if(data.error == 1001){
+          $form.data("bootstrapValidator").updateStatus("password","INVALID","callback");
+        }
+      }
+    })
+  });
+     //重置样式
+     $("[type='reset']").on("click",function(){
+      $form.data("bootstrapValidator").resetForm();
+    });
+});
+
+
